f1b3df7b236566cf27407eb1aa1fbd0c75493ad8
MayFairMI6 <143886061+MayFairMI6@users.noreply.github.com>
2024-11-10 08:36:47 +0800

    Update SpendSwift.java

--- src/main/java/seedu/spendswift/SpendSwift.java
+++ src/main/java/seedu/spendswift/SpendSwift.java
@@ -6,6 +6,7 @@ import seedu.spendswift.command.CategoryManager;
 import seedu.spendswift.command.ExpenseManager;
 import seedu.spendswift.command.TrackerData;
 import seedu.spendswift.parser.Parser;
+import seedu.spendswift.CurrencyConverter;
 
 import java.io.IOException;
 import java.util.Scanner;
