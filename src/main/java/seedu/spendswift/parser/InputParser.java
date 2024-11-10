c3c604e518dfdd128e4f3412fdf40b4d71e93d3c
MayFairMI6 <143886061+MayFairMI6@users.noreply.github.com>
2024-11-10 12:42:14 +0800

    Update InputParser.java

--- src/main/java/seedu/spendswift/parser/InputParser.java
+++ src/main/java/seedu/spendswift/parser/InputParser.java
@@ -44,6 +44,43 @@ public class InputParser {
 
         return currency.toUpperCase();
     }
+
+    public String parseName(String input) {
+        return parseComponent(input, "n/");
+    }
+
+    public String parseCategory(String input) {
+        return parseComponent(input, "c/");
+    }
+    public double parseLimit(String input) {
+        String limitStr = parseComponent(input, "l/");
+        try {
+            return Double.parseDouble(limitStr);
+        } catch (NumberFormatException e) {
+            ErrorMessage.printInvalidLimit();
+            return Double.NaN;
+        }
+    }
+
+    public double parseAmount(String input) {
+        String amountStr = parseComponent(input, "a/");
+        try {
+            return Double.parseDouble(amountStr);
+        } catch (NumberFormatException e) {
+            ErrorMessage.printInvalidAmount();
+            return Double.NaN;
+        }
+    }
+   
+    public int parseIndex(String input) {
+        String indexStr = parseComponent(input, "e/");
+        try {
+            return Integer.parseInt(indexStr) - 1;
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
 }
 
 
