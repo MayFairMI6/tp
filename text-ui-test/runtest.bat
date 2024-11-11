d934fa9f672e825e18f25b6c7ffe9fa9dd636276
MayFairMI6 <143886061+MayFairMI6@users.noreply.github.com>
2024-11-12 02:03:58 +0800

    Update runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,15 +1,34 @@
 @echo off
+REM Change to script directory
+cd /d %~dp0
+
+REM Go to the project root and build the JAR file
 cd ..
 call gradlew clean shadowJar
 
+REM Go to the test directory
 cd text-ui-test
-java -jar ..\build\libs\*.jar < input.txt > ACTUAL.TXT
 
-REM Convert EXPECTED.TXT and ACTUAL.TXT to Unix line endings
-powershell -Command "(Get-Content EXPECTED.TXT) -replace '\r\n', '`n' | Set-Content EXPECTED-UNIX.TXT"
-powershell -Command "(Get-Content ACTUAL.TXT) -replace '\r\n', '`n' | Set-Content ACTUAL-UNIX.TXT"
+REM Run the JAR with input and save the output
+for /f %%i in ('dir /b ..\build\libs\*.jar') do (
+    java -jar ..\build\libs\%%i < input.txt > ACTUAL.TXT
+    goto :done
+)
+:done
 
-fc /W EXPECTED-UNIX.TXT ACTUAL-UNIX.TXT
+REM Prepare the expected file
+copy /Y EXPECTED.TXT EXPECTED-UNIX.TXT
+
+REM Convert line endings to Unix format using PowerShell
+powershell -Command "Get-Content EXPECTED-UNIX.TXT | ForEach-Object {$_ -replace '`r`n', '`n'} | Set-Content EXPECTED-UNIX.TXT"
+powershell -Command "Get-Content ACTUAL.TXT | ForEach-Object {$_ -replace '`r`n', '`n'} | Set-Content ACTUAL.TXT"
+
+REM Remove lines with underscores
+powershell -Command "Get-Content EXPECTED-UNIX.TXT | Where-Object {$_ -notmatch '^_+$'} | Set-Content EXPECTED-CLEAN.TXT"
+powershell -Command "Get-Content ACTUAL.TXT | Where-Object {$_ -notmatch '^_+$'} | Set-Content ACTUAL-CLEAN.TXT"
+
+REM Compare the cleaned files
+fc /W EXPECTED-CLEAN.TXT ACTUAL-CLEAN.TXT > NUL
 if %errorlevel% equ 0 (
     echo Test passed!
     exit /b 0
