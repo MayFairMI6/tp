9e49e53128b27d4bb7a9e030591f45f20f8d5eea
MayFairMI6 <143886061+MayFairMI6@users.noreply.github.com>
2024-11-12 01:43:42 +0800

    Update runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
-# change to script directory
+# Change to script directory
 cd "${0%/*}"
 
+# Go to the project root and build the JAR file
 cd ..
 ./gradlew clean shadowJar
 
+# Go to the test directory
 cd text-ui-test
 
-java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
+# Run the JAR with input and save the output
+java -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
 
+# Prepare the expected file and convert line endings
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
+
+# Remove any remaining \r characters in case dos2unix didn’t fully convert
+sed -i 's/\r$//' EXPECTED-UNIX.TXT ACTUAL.TXT
+
+# Compare the files
 diff EXPECTED-UNIX.TXT ACTUAL.TXT
+
+# Check the result of diff
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
